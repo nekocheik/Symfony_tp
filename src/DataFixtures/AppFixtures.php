1b0781af12b3d2eed4f116f5d5e836cce49a41b6
Antoine07 <antoine.lucsko@gmail.com>
2021-02-12 10:54:02 +0100

    faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -15,11 +15,25 @@ class AppFixtures extends Fixture
     {
         $faker = Faker\Factory::create('fr_FR');
 
-        // create 20 beers! Bam!
+        $names = [
+            'beer super',
+            'beer cool',
+            'beer strange',
+            'beer very bad trip',
+            'beer super strange',
+            'beer very sweet',
+            'beer hyper cool',
+            'beer without alcool',
+            'beer simple',
+            'beer very simple',
+        ];
+
         for ($i = 0; $i < 20; $i++) {
             $beer = new Beer();
-            $beer->setName('product '.$i);
-           
+            $beer->setName($names[random_int(0, count($names) - 1)]);
+            // si vous n'avez pas la version de PHP 8 ceci marchera 
+            $beer->setDescription($faker->paragraph($nbSentences = 3, $variableNbSentences = true));
+
             $manager->persist($beer);
         }
 
